55e20cdef16fa502d85038eaa75a331a9b797279
Carlos Lapao <cjlapao@gmail.com>
2023-10-24 12:42:11 +0100

    changed version

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	versionSvc.License = "MIT"
 	versionSvc.Minor = 1
 	versionSvc.Major = 0
-	versionSvc.Build = 28
+	versionSvc.Build = 29
 
 	if helper.GetFlagSwitch("version", false) {
 		println(versionSvc.String())
