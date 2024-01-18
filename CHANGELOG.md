fff101192590a34a19d7e8fa4dc826166c2e3e6b
Carlos Lapao <cjlapao@gmail.com>
2024-01-18 08:44:35 +0000

    improved logging (#78)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.6] -
+
+### Fixed
+
+- Fixed an issue where the api base context was not setting the log correctly
+  resulting in missing log lines
+
 ## [0.4.5] - 2024-01-16
 
 ### Added
---
+++ src/basecontext/api_context.go
@@ -0,0 +1,22 @@
+package basecontext
+
+import (
+	"context"
+
+	"github.com/Parallels/pd-api-service/models"
+)
+
+type ApiContext interface {
+	Context() context.Context
+	GetAuthorizationContext() *AuthorizationContext
+	GetRequestId() string
+	GetUser() *models.ApiUser
+	Verbose() bool
+	EnableLog()
+	DisableLog()
+	ToggleLogTimestamps(value bool)
+	LogInfo(format string, a ...interface{})
+	LogError(format string, a ...interface{})
+	LogDebug(format string, a ...interface{})
+	LogWarn(format string, a ...interface{})
+}
