3298d8d59dd13cbc71b958c7d5a0402a9dcc0043
Carlos Lapao <cjlapao@gmail.com>
2024-02-19 15:04:26 +0000

    Release extension version 0.5.3 (#126)

--- src/main.go
+++ src/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 var (
-	ver        = "0.5.2"
+	ver        = "0.5.3"
 	versionSvc = version.Get()
 )
 
 //	@title			Parallels Desktop API
-//	@version		0.5.2
+//	@version		0.5.3
 //	@description	Parallels Desktop API Service
 //	@termsOfService	http://swagger.io/terms/
 
