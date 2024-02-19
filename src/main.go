60babab14e9423d9ea548219ccffff34f09c92f1
cjlapao <cjlapao@gmail.com>
2024-02-19 07:31:28 +0000

    Release extension version 0.5.0

--- src/main.go
+++ src/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 var (
-	ver        = "0.4.5"
+	ver        = "0.5.0"
 	versionSvc = version.Get()
 )
 
 //	@title			Parallels Desktop API
-//	@version		0.4.6
+//	@version		0.5.0
 //	@description	Parallels Desktop API Service
 //	@termsOfService	http://swagger.io/terms/
 
