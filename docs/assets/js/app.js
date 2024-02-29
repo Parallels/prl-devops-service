f18ef26b3733b7d89815ebc5005fa2d911a7ea32
Carlos Lapao <cjlapao@gmail.com>
2024-02-29 15:47:06 +0000

    attempting automation in stars

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -11,3 +11,18 @@ function openTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " is-active";
 }
+
+var getJSON = function(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
