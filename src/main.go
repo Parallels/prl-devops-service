93e7f840ed54a98483e34f25ff95d3e684cedcbc
Carlos Lapao <cjlapao@gmail.com>
2024-09-05 09:45:41 +0100

    Release extension version 0.9.0 (#189)

--- src/main.go
+++ src/main.go
@@ -19,10 +19,10 @@ import (
 	"github.com/cjlapao/common-go/version"
 )
 
-var ver = "0.8.8"
+var ver = "0.9.0"
 
 //	@title			Parallels Desktop DevOps Service
-//	@version		0.8.8
+//	@version		0.9.0
 //	@description	Parallels Desktop DevOps Service
 //	@termsOfService	http://swagger.io/terms/
 
