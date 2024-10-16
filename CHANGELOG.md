5e6799f0c195f66b081f3731244bb227b0cea0cd
cjlapao <cjlapao@gmail.com>
2024-10-16 08:24:59 +0000

    Release extension version 0.9.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 All notable changes to this project will be documented in this file.
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.9.7] - 2024-10-16
+
+
+
 ## [0.9.6] - 2024-09-19
 
 ### Fixed
--- src/main.go
+++ src/main.go
@@ -20,10 +20,10 @@ import (
 	"github.com/cjlapao/common-go/version"
 )
 
-var ver = "0.9.6.1"
+var ver = "0.9.7"
 
 //	@title			Parallels Desktop DevOps Service
-//	@version		0.9.6
+//	@version		0.9.7
 //	@description	Parallels Desktop DevOps Service
 //	@termsOfService	http://swagger.io/terms/
 
@@ -39,10 +39,10 @@ var ver = "0.9.6.1"
 //	@in							header
 //	@name						X-Api-Key
 
-// @securityDefinitions.apikey	BearerAuth
-// @description				Type "Bearer" followed by a space and JWT token.
-// @in							header
-// @name						Authorization
+//	@securityDefinitions.apikey	BearerAuth
+//	@description				Type "Bearer" followed by a space and JWT token.
+//	@in							header
+//	@name						Authorization
 func main() {
 	// catching all of the exceptions
 	defer func() {
