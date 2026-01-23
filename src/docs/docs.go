7e7f307457c82a2f437cc2d3e30e04f2ea22932e
cjlapao <cjlapao@gmail.com>
2026-01-23 17:04:29 +0000

    Release extension version 0.9.16

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -10286,7 +10286,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.9.15",
+	Version:          "0.9.16",
 	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
