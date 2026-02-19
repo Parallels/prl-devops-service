907e6f0cd194d40cfd63a3d110bd7cc9747ae9f8
Sai Kumar Peddireddy <35918740+sai-kumar-peddireddy@users.noreply.github.com>
2026-02-19 17:03:24 +0100

    Enhance VM Retrieval by Implementing Delayed Cache Lookup Before Fallback to Non-Cached Method

--- src/serviceprovider/parallelsdesktop/helpers.go
+++ src/serviceprovider/parallelsdesktop/helpers.go
@@ -2,58 +2,73 @@ package parallelsdesktop
 
 import (
 	"strings"
+	"time"
 
 	"github.com/Parallels/prl-devops-service/basecontext"
 	"github.com/Parallels/prl-devops-service/models"
 )
 
-func (s *ParallelsService) findVm(ctx basecontext.ApiContext, idOrName string, cached bool) (*models.ParallelsVM, error) {
-	var err error
-	var vms []models.ParallelsVM
-	if cached {
-		vms, err = s.GetCachedVms(ctx, "")
-	} else {
-		vms, err = s.GetVms(ctx, "")
-	}
-	if err != nil {
-		return nil, err
-	}
+const CacheFindNumberOfRetries = 10
+const CacheFindRetryDelay = 500 * time.Millisecond
 
-	for _, vm := range vms {
-		if strings.EqualFold(vm.Name, idOrName) || strings.EqualFold(vm.ID, idOrName) {
-			return &vm, nil
+func (s *ParallelsService) findVmInCacheAndSystem(ctx basecontext.ApiContext, idOrName string) (*models.ParallelsVM, error) {
+
+	for i := 0; i < CacheFindNumberOfRetries; i++ {
+		s.RLock()
+		vms := s.cachedLocalVms
+		s.RUnlock()
+		for _, vm := range vms {
+			if strings.EqualFold(vm.Name, idOrName) || strings.EqualFold(vm.ID, idOrName) {
+				return &vm, nil
+			}
+		}
+		ctx.LogInfof("VM with name or id %v not found in cache, retrying... (%d/%d)", idOrName, i+1, CacheFindNumberOfRetries)
+		time.Sleep(CacheFindRetryDelay)
+	}
+	// To fetch all virtual machines (VMs), we intentionally call `GetVms` without specifying an ID or name.
+	// This process might take some time because our goal is to refresh the entire cache.
+	// If we find any VMs, it means the current cache is outdated, and we will proceed to update it.
+	vms, err := s.GetVms(ctx, "")
+	if err == nil {
+		for _, vm := range vms {
+			if strings.EqualFold(vm.Name, idOrName) || strings.EqualFold(vm.ID, idOrName) {
+				ctx.LogWarnf("Vm is not present in cache but found in machine updating cache")
+				s.Lock()
+				s.cachedLocalVms = vms
+				s.Unlock()
+				return &vm, nil
+			}
 		}
 	}
-	ctx.LogInfof("VM with name or id %v not found with cached=%v", idOrName, cached)
 	return nil, ErrVirtualMachineNotFound
 }
 
-func (s *ParallelsService) findVmInCacheAndSystem(ctx basecontext.ApiContext, idOrName string) (*models.ParallelsVM, error) {
-	vm, err := s.findVmSync(ctx, idOrName, true)
-	if err == nil {
-		return vm, nil
-	}
-	return s.findVmSync(ctx, idOrName, false)
+func (s *ParallelsService) findVmSync(ctx basecontext.ApiContext, idOrName string) (*models.ParallelsVM, error) {
+	return s.findVmInCacheAndSystem(ctx, idOrName)
 }
 
-func (s *ParallelsService) findVmSync(ctx basecontext.ApiContext, idOrName string, cached bool) (*models.ParallelsVM, error) {
-	var err error
-	var vms []models.ParallelsVM
-	if cached {
-		vms, err = s.GetCachedVms(ctx, "")
-	} else {
-		vms, err = s.GetVms(ctx, "")
+func (s *ParallelsService) findVmWithStateInCacheAndSystem(ctx basecontext.ApiContext, idOrName string, state string) (*models.ParallelsVM, error) {
+	for i := 0; i < CacheFindNumberOfRetries; i++ {
+		s.RLock()
+		vms := s.cachedLocalVms
+		s.RUnlock()
+		for _, vm := range vms {
+			if (strings.EqualFold(vm.Name, idOrName) || strings.EqualFold(vm.ID, idOrName)) && strings.EqualFold(vm.State, state) {
+				return &vm, nil
+			}
+		}
+		ctx.LogInfof("VM with name or id %v and state %v not found in cache, retrying... (%d/%d)", idOrName, state, i+1, CacheFindNumberOfRetries)
+		time.Sleep(CacheFindRetryDelay)
 	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, vm := range vms {
-		if strings.EqualFold(vm.Name, idOrName) || strings.EqualFold(vm.ID, idOrName) {
+	vm, err := s.getVmForCurrentUser(ctx, idOrName)
+	if err == nil {
+		if (strings.EqualFold(vm.Name, idOrName) || strings.EqualFold(vm.ID, idOrName)) && strings.EqualFold(vm.State, state) {
+			ctx.LogWarnf("Vm with desired state is not present in cache but found in machine, updating cache")
+			go func() {
+				s.refreshCache(ctx)
+			}()
 			return &vm, nil
 		}
 	}
-	ctx.LogInfof("VM with name or id %v not found with cached=%v", idOrName, cached)
 	return nil, ErrVirtualMachineNotFound
 }
