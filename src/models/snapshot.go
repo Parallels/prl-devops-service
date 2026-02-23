9cd3a4b357c4295777a6821630c8b4a701b6e73b
Mukhe Siva Parvathi <parvathi3112@gmail.com>
2026-02-23 11:42:16 +0100

    feat: Implemented comprehensive virtual machine (#378)
    snapshot management endpoints.
    
    Co-authored-by: Carlos Lapao <cjlapao@gmail.com>

---
+++ src/models/snapshot.go
@@ -0,0 +1,79 @@
+package models
+
+import (
+	"github.com/Parallels/prl-devops-service/errors"
+)
+
+type CreateSnapShotRequest struct {
+	VMId                string `json:"vm_id"`
+	VMName              string `json:"vm_name"`
+	SnapshotName        string `json:"snapshot_name,omitempty"`
+	SnapshotDescription string `json:"snapshot_description,omitempty"`
+}
+
+func (r *CreateSnapShotRequest) Validate() error {
+
+	if r.VMId == "" && r.VMName == "" {
+		return errors.New("vm_id and vm_name cannot be empty")
+	}
+
+	return nil
+}
+
+type DeleteSnapshotRequest struct {
+	VMId      string `json:"vm_id"`
+	VMName    string `json:"vm_name"`
+	ChildName string `json:"child_name,omitempty"`
+}
+
+func (r *DeleteSnapshotRequest) Validate() error {
+
+	if r.VMId == "" && r.VMName == "" {
+		return errors.New("vm_id and vm_name cannot be empty")
+	}
+
+	return nil
+}
+
+type ListSnapshotRequest struct {
+	VMId   string `json:"vm_id"`
+	VMName string `json:"vm_name"`
+}
+
+func (r *ListSnapshotRequest) Validate() error {
+
+	if r.VMId == "" && r.VMName == "" {
+		return errors.New("vm_id and vm_name cannot be empty")
+	}
+
+	return nil
+}
+
+type RevertSnapshotRequest struct {
+	VMId   string `json:"vm_id"`
+	VMName string `json:"vm_name"`
+}
+
+func (r *RevertSnapshotRequest) Validate() error {
+
+	if r.VMId == "" && r.VMName == "" {
+		return errors.New("vm_id and vm_name cannot be empty")
+	}
+
+	return nil
+}
+
+type SwitchSnapshotRequest struct {
+	VMId       string `json:"vm_id"`
+	VMName     string `json:"vm_name"`
+	SkipResume bool   `json:"skip_resume,omitempty"`
+}
+
+func (r *SwitchSnapshotRequest) Validate() error {
+
+	if r.VMId == "" && r.VMName == "" {
+		return errors.New("vm_id and vm_name cannot be empty")
+	}
+
+	return nil
+}
