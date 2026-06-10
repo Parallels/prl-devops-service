5e57d6cb9dc5c3c734572b0260e4ae1ce49714ed
Siva Parvathi <parvathi3112@gmail.com>
2026-06-10 13:23:26 +0200

    wip

--- src/data/models/api_key.go
+++ src/data/models/api_key.go
@@ -1,17 +1,17 @@
 package models
 
 type ApiKey struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	Key       string `json:"key"`
-	Secret    string `json:"secret"`
-	Revoked   bool   `json:"revoked"`
+	ID        string `json:"id" gorm:"primaryKey"`
+	Name      string `json:"name" gorm:"not null"`
+	Key       string `json:"key" gorm:"unique;not null"`
+	Secret    string `json:"secret" gorm:"not null"`
+	Revoked   bool   `json:"revoked" gorm:"default:false"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 	RevokedAt string `json:"revoked_at"`
 	ExpiresAt string `json:"expires_at"`
-	UserID    string `json:"user_id,omitempty"`
-	*DbRecord `json:"db_record"`
+	UserID    string `json:"user_id,omitempty" gorm:"index"`
+	*DbRecord `json:"db_record" gorm:"embedded"`
 }
 
 func (a ApiKey) GetUserID() string {
--- src/data/models/role.go
+++ src/data/models/role.go
@@ -1,8 +1,8 @@
 package models
 
 type Role struct {
-	ID          string  `json:"id,omitempty"`
-	Name        string  `json:"name"`
+	ID          string  `json:"id,omitempty" gorm:"primaryKey"`
+	Name        string  `json:"name" gorm:"not null"`
 	Description string  `json:"description,omitempty"`
 	Internal    bool    `json:"internal"`
 	Claims      []Claim `json:"claims,omitempty"`
