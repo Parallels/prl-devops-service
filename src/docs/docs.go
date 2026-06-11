76370c4855b49446f2da29b887c7256075c377db
Carlos Lapao <cjlapao@gmail.com>
2026-06-11 11:09:24 +0100

    Release extension version 1.0.2 (#498)

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -14297,7 +14297,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.0.1",
+	Version:          "1.0.2",
 	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
