2ee922a0028638563b5284a2323264381582aab5
Siva Parvathi <parvathi3112@gmail.com>
2026-06-12 13:29:08 +0200

    feat: add GORM tags  for virtual machines, snapshots, and packer templates

--- src/data/models/VM_Snapshots.go
+++ src/data/models/VM_Snapshots.go
@@ -1,12 +1,12 @@
 package models
 
 type VMSnapshot struct {
-	ID      string `json:"id,omitempty"`
-	Name    string `json:"name"`
-	Date    string `json:"date"`
-	State   string `json:"state"`
-	Current bool   `json:"current"`
-	Parent  string `json:"parent"`
+	ID      string `json:"id,omitempty" gorm:"column:id;type:varchar(255)"`
+	Name    string `json:"name" gorm:"column:name;type:varchar(255)"`
+	Date    string `json:"date" gorm:"column:date;type:varchar(255)"`
+	State   string `json:"state" gorm:"column:state;type:varchar(255)"`
+	Current bool   `json:"current" gorm:"column:current;type:boolean"`
+	Parent  string `json:"parent" gorm:"column:parent;type:varchar(255)"`
 }
 
 type VMSnapshots struct {
--- src/data/models/packer_template.go
+++ src/data/models/packer_template.go
@@ -5,21 +5,21 @@ import (
 )
 
 type PackerTemplate struct {
-	ID             string            `json:"id" gorm:"primaryKey"`
-	Name           string            `json:"name" gorm:"not null;type:varchar(255);"`
-	Owner          string            `json:"owner" gorm:"not null;type:varchar(255);"`
-	Hostname       string            `json:"hostname" gorm:"not null;type:varchar(255);"`
-	Description    string            `json:"description"`
-	PackerFolder   string            `json:"packer_folder"`
-	Variables      map[string]string `json:"variables" gorm:"serializer:json"`
-	Addons         []string          `json:"addons" gorm:"serializer:json"`
-	Specs          map[string]string `json:"specs" gorm:"serializer:json"`
-	Defaults       map[string]string `json:"defaults" gorm:"serializer:json"`
-	Internal       bool              `json:"internal,omitempty" gorm:"default:false"`
-	UpdatedAt      string            `json:"updated_at,omitempty"`
-	CreatedAt      string            `json:"created_at,omitempty"`
-	RequiredRoles  []string          `json:"required_roles,omitempty" gorm:"serializer:json"`
-	RequiredClaims []string          `json:"required_claims,omitempty" gorm:"serializer:json"`
+	ID             string            `json:"id" gorm:"column:id;primaryKey"`
+	Name           string            `json:"name" gorm:"column:name;not null;type:varchar(255);"`
+	Owner          string            `json:"owner" gorm:"column:owner;not null;type:varchar(255);"`
+	Hostname       string            `json:"hostname" gorm:"column:hostname;not null;type:varchar(255);"`
+	Description    string            `json:"description" gorm:"column:description"`
+	PackerFolder   string            `json:"packer_folder" gorm:"column:packer_folder"`
+	Variables      map[string]string `json:"variables" gorm:"column:variables;serializer:json"`
+	Addons         []string          `json:"addons" gorm:"column:addons;serializer:json"`
+	Specs          map[string]string `json:"specs" gorm:"column:specs;serializer:json"`
+	Defaults       map[string]string `json:"defaults" gorm:"column:defaults;serializer:json"`
+	Internal       bool              `json:"internal,omitempty" gorm:"column:internal;default:false"`
+	UpdatedAt      string            `json:"updated_at,omitempty" gorm:"column:updated_at"`
+	CreatedAt      string            `json:"created_at,omitempty" gorm:"column:created_at"`
+	RequiredRoles  []string          `json:"required_roles,omitempty" gorm:"column:required_roles;serializer:json"`
+	RequiredClaims []string          `json:"required_claims,omitempty" gorm:"column:required_claims;serializer:json"`
 }
 
 func (m *PackerTemplate) Validate() error {
