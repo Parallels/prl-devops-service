a66166a7186a6052eeba68c0716815455b7433d8
Siva Parvathi <parvathi3112@gmail.com>
2026-06-15 16:50:13 +0200

    feat: add GORM tags  for orchestrator_host.go, orchestrator_enrollment_token.go, job.go.

--- src/data/models/orchestrator_enrollment_token.go
+++ src/data/models/orchestrator_enrollment_token.go
@@ -1,11 +1,11 @@
 package models
 
 type OrchestratorEnrollmentToken struct {
-	ID        string `json:"id"`
-	Token     string `json:"token"`
-	HostName  string `json:"host_name"`
-	Used      bool   `json:"used"`
-	ExpiresAt string `json:"expires_at"`
-	CreatedAt string `json:"created_at"`
-	*DbRecord `json:"db_record"`
+	ID        string `json:"id" gorm:"primaryKey;column:id;type:varchar(255)"`
+	Token     string `json:"token" gorm:"column:token;type:varchar(255)"`
+	HostName  string `json:"host_name" gorm:"column:host_name;type:varchar(255)"`
+	Used      bool   `json:"used" gorm:"column:used;type:boolean;default:false;not null"`
+	ExpiresAt string `json:"expires_at" gorm:"column:expires_at;type:timestamp"`
+	CreatedAt string `json:"created_at" gorm:"column:created_at;type:timestamp"`
+	*DbRecord `json:"db_record"  gorm:"embedded"`
 }
