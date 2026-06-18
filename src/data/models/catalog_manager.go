557c50f4f9fd2a4cf92915a16d41a6b08b9b9616
Siva Parvathi <parvathi3112@gmail.com>
2026-06-18 14:56:10 +0200

    feat: Add GORM tags to catalog_manager,catalog_manifest,reverse_proxy files.

--- src/data/models/catalog_manager.go
+++ src/data/models/catalog_manager.go
@@ -1,18 +1,18 @@
 package models
 
 type CatalogManager struct {
-	ID                   string   `json:"id" yaml:"id"`
-	Name                 string   `json:"name" yaml:"name"`
-	URL                  string   `json:"url" yaml:"url"`
-	Internal             bool     `json:"internal" yaml:"internal"`
-	Active               bool     `json:"active" yaml:"active"`
-	AuthenticationMethod string   `json:"authentication_method" yaml:"authentication_method"`
-	Username             string   `json:"username,omitempty" yaml:"username,omitempty"`
-	Password             string   `json:"password,omitempty" yaml:"password,omitempty"`
-	ApiKey               string   `json:"api_key,omitempty" yaml:"api_key,omitempty"`
-	Global               bool     `json:"global" yaml:"global"`
-	RequiredClaims       []string `json:"required_claims,omitempty" yaml:"required_claims,omitempty"`
-	OwnerID              string   `json:"owner_id" yaml:"owner_id"`
-	CreatedAt            string   `json:"created_at" yaml:"created_at"`
-	UpdatedAt            string   `json:"updated_at" yaml:"updated_at"`
+	ID                   string   `json:"id" yaml:"id" gorm:"primaryKey;type:varchar(255);column:id;not null"`
+	Name                 string   `json:"name" yaml:"name" gorm:"column:name;not null;type:varchar(255);index"`
+	URL                  string   `json:"url" yaml:"url" gorm:"column:url;not null;type:varchar(255);index"`
+	Internal             bool     `json:"internal" yaml:"internal" gorm:"column:internal;not null;type:boolean;default:0"`
+	Active               bool     `json:"active" yaml:"active" gorm:"column:active;not null;type:boolean;default:0"`
+	AuthenticationMethod string   `json:"authentication_method" yaml:"authentication_method" gorm:"column:authentication_method;not null;type:varchar(255)"`
+	Username             string   `json:"username,omitempty" yaml:"username,omitempty" gorm:"column:username;type:varchar(255)"`
+	Password             string   `json:"password,omitempty" yaml:"password,omitempty" gorm:"column:password;type:varchar(255)"`
+	ApiKey               string   `json:"api_key,omitempty" yaml:"api_key,omitempty" gorm:"column:api_key;type:varchar(255)"`
+	Global               bool     `json:"global" yaml:"global" gorm:"column:global;not null;type:boolean;default:0"`
+	RequiredClaims       []string `json:"required_claims,omitempty" yaml:"required_claims,omitempty" gorm:"column:required_claims;type:json;serializer:json"`
+	OwnerID              string   `json:"owner_id" yaml:"owner_id" gorm:"column:owner_id;type:varchar(255)"`
+	CreatedAt            string   `json:"created_at" yaml:"created_at" gorm:"column:created_at;type:timestamp"`
+	UpdatedAt            string   `json:"updated_at" yaml:"updated_at" gorm:"column:updated_at;type:timestamp"`
 }
--- src/data/models/catalog_manifest.go
+++ src/data/models/catalog_manifest.go
@@ -3,39 +3,39 @@ package models
 import "strings"
 
 type CatalogManifest struct {
-	ID                      string                       `json:"id"`
-	CatalogId               string                       `json:"catalog_id"`
-	Name                    string                       `json:"name"`
-	Version                 string                       `json:"version"`
-	Architecture            string                       `json:"architecture"`
-	Description             string                       `json:"description"`
-	Path                    string                       `json:"path,omitempty"`
-	PackFile                string                       `json:"pack_path,omitempty"`
-	MetadataFile            string                       `json:"metadata_path,omitempty"`
-	Type                    string                       `json:"type"`
-	Provider                *CatalogManifestProvider     `json:"provider"`
-	Size                    int64                        `json:"size"`
-	RequiredRoles           []string                     `json:"required_roles"`
-	RequiredClaims          []string                     `json:"required_claims"`
-	Tags                    []string                     `json:"tags"`
-	CreatedAt               string                       `json:"created_at"`
-	UpdatedAt               string                       `json:"updated_at"`
-	LastDownloadedAt        string                       `json:"last_downloaded_at"`
-	LastDownloadedUser      string                       `json:"last_downloaded_user"`
-	IsCompressed            bool                         `json:"is_compressed"`
-	PackRelativePath        string                       `json:"pack_relative_path"`
-	DownloadCount           int                          `json:"download_count"`
-	VirtualMachineContents  []CatalogManifestContentItem `json:"virtual_machine_contents"`
-	PackContents            []CatalogManifestContentItem `json:"pack_contents"`
-	PackSize                int64                        `json:"pack_size,omitempty"`
-	MinimumSpecRequirements *MinimumSpecRequirement      `json:"minimum_requirements,omitempty"`
-	Tainted                 bool                         `json:"tainted"`
-	TaintedBy               string                       `json:"tainted_by"`
-	UnTaintedBy             string                       `json:"untainted_by"`
-	TaintedAt               string                       `json:"tainted_at"`
-	Revoked                 bool                         `json:"revoked"`
-	RevokedBy               string                       `json:"revoked_by"`
-	RevokedAt               string                       `json:"revoked_at"`
+	ID                      string                       `json:"id" gorm:"primaryKey;type:varchar(255);column:id;not null"`
+	CatalogId               string                       `json:"catalog_id" gorm:"column:catalog_id;not null;type:varchar(255);index"`
+	Name                    string                       `json:"name" gorm:"column:name;not null;type:varchar(255);index"`
+	Version                 string                       `json:"version" gorm:"column:version;not null;type:varchar(255)"`
+	Architecture            string                       `json:"architecture" gorm:"column:architecture;not null;type:varchar(255)"`
+	Description             string                       `json:"description" gorm:"column:description;not null;type:text"`
+	Path                    string                       `json:"path,omitempty" gorm:"column:path;type:varchar(255)"`
+	PackFile                string                       `json:"pack_path,omitempty" gorm:"column:pack_path;type:varchar(255)"`
+	MetadataFile            string                       `json:"metadata_path,omitempty" gorm:"column:metadata_path;type:varchar(255)"`
+	Type                    string                       `json:"type" gorm:"column:type;not null;type:varchar(255);index"`
+	Provider                *CatalogManifestProvider     `json:"provider" gorm:"column:provider;type:json;serializer:json"`
+	Size                    int64                        `json:"size" gorm:"column:size;not null;type:bigint"`
+	RequiredRoles           []string                     `json:"required_roles" gorm:"column:required_roles;type:json;serializer:json"`
+	RequiredClaims          []string                     `json:"required_claims" gorm:"column:required_claims;type:json;serializer:json"`
+	Tags                    []string                     `json:"tags" gorm:"column:tags;type:json;serializer:json"`
+	CreatedAt               string                       `json:"created_at" gorm:"column:created_at;not null;type:timestamp"`
+	UpdatedAt               string                       `json:"updated_at" gorm:"column:updated_at;not null;type:timestamp"`
+	LastDownloadedAt        string                       `json:"last_downloaded_at" gorm:"column:last_downloaded_at;type:timestamp"`
+	LastDownloadedUser      string                       `json:"last_downloaded_user" gorm:"column:last_downloaded_user;type:varchar(255)"`
+	IsCompressed            bool                         `json:"is_compressed" gorm:"column:is_compressed;type:boolean;default:0"`
+	PackRelativePath        string                       `json:"pack_relative_path" gorm:"column:pack_relative_path;type:varchar(255)"`
+	DownloadCount           int                          `json:"download_count" gorm:"column:download_count;type:int"`
+	VirtualMachineContents  []CatalogManifestContentItem `json:"virtual_machine_contents" gorm:"column:virtual_machine_contents;type:json;serializer:json"`
+	PackContents            []CatalogManifestContentItem `json:"pack_contents" gorm:"column:pack_contents;type:json;serializer:json"`
+	PackSize                int64                        `json:"pack_size,omitempty" gorm:"column:pack_size;type:bigint"`
+	MinimumSpecRequirements *MinimumSpecRequirement      `json:"minimum_requirements,omitempty" gorm:"column:minimum_requirements;type:json;serializer:json"`
+	Tainted                 bool                         `json:"tainted" gorm:"column:tainted;type:boolean;default:0"`
+	TaintedBy               string                       `json:"tainted_by" gorm:"column:tainted_by;type:varchar(255)"`
+	UnTaintedBy             string                       `json:"untainted_by" gorm:"column:untainted_by;type:varchar(255)"`
+	TaintedAt               string                       `json:"tainted_at" gorm:"column:tainted_at;type:timestamp"`
+	Revoked                 bool                         `json:"revoked" gorm:"column:revoked;type:boolean;default:0"`
+	RevokedBy               string                       `json:"revoked_by" gorm:"column:revoked_by;type:varchar(255)"`
+	RevokedAt               string                       `json:"revoked_at" gorm:"column:revoked_at;type:timestamp"`
 }
 
 func (r *CatalogManifest) AddTag(tag string) {
