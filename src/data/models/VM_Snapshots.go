9a8d97e3ac4ca1c5b79643ad135b3f362c47a8d6
Siva Parvathi <parvathi3112@gmail.com>
2026-07-01 19:27:29 +0200

    feat:Add createdby field for vm to know who created this vm.

--- src/data/models/VM_Snapshots.go
+++ src/data/models/VM_Snapshots.go
@@ -1,12 +1,14 @@
 package models
 
 type VMSnapshot struct {
-	ID      string `json:"id,omitempty" gorm:"primaryKey;column:id;type:varchar(64);not null"`
-	Name    string `json:"name" gorm:"column:name;type:varchar(255);not null"`
-	Date    string `json:"date" gorm:"column:date;type:timestamp;not null"`
-	State   string `json:"state" gorm:"column:state;type:varchar(32);not null"`
-	Current bool   `json:"current" gorm:"column:current;type:boolean;not null"`
-	Parent  string `json:"parent" gorm:"column:parent;type:varchar(64);not null"`
+	ID            string `json:"id,omitempty" gorm:"primaryKey;column:id;type:varchar(64);not null"`
+	Name          string `json:"name" gorm:"column:name;type:varchar(255);not null"`
+	Date          string `json:"date" gorm:"column:date;type:timestamp;not null"`
+	State         string `json:"state" gorm:"column:state;type:varchar(32);not null"`
+	Current       bool   `json:"current" gorm:"column:current;type:boolean;not null"`
+	Parent        string `json:"parent" gorm:"column:parent;type:varchar(64);not null"`
+	CreatedBy     string `json:"created_by,omitempty" gorm:"column:created_by;type:varchar(64)"`
+	CreatedByUser *User  `json:"created_by_user,omitempty" gorm:"foreignKey:CreatedBy;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type VMSnapshots struct {
